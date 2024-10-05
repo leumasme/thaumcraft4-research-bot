407d41a660f78c00f632710b284383081940e858
Temm <samumert@gmail.com>
2024-10-06 00:21:54 +0200

    fix edgecase when only 2 aspects are on the board

--- src/thaumcraft4_research_bot/utils/solvers/ringsolver.py
+++ src/thaumcraft4_research_bot/utils/solvers/ringsolver.py
@@ -1,13 +1,13 @@
 from thaumcraft4_research_bot.utils.grid import HexGrid, SolvingHexGrid
 from thaumcraft4_research_bot.utils.aspects import calculate_cost_of_aspect_path
-from typing import Tuple, List
+from typing import Tuple, List, Dict
 
 
 def solve(grid: HexGrid, start_aspects: List[Tuple[int, int]]) -> SolvingHexGrid:
     solving = SolvingHexGrid.from_hexgrid(grid)
 
     # create paths_to_connect by finding the 2 closest neighbors of each aspect via pathfinding.
-    closest_neighbors = {}
+    closest_neighbors: Dict[Tuple[int, int], List[Tuple[Tuple[int, int], int]]] = {}
     for start_aspect in start_aspects:
         neigh_paths = []  # How far is it to each of the other aspects?
         for other in start_aspects:
@@ -24,6 +24,9 @@ def solve(grid: HexGrid, start_aspects: List[Tuple[int, int]]) -> SolvingHexGrid
         # Take closest 2 other aspects and store
         neigh_paths.sort(key=lambda x: x[1])
         if len(neigh_paths) < 2:
+            if len(start_aspects) == 2:
+                # If there's only one pair of aspects, we don't need to do this
+                break
             raise Exception(
                 "Could not find 2 neighbors for", start_aspect, solving.grid
             )
@@ -34,18 +37,28 @@ def solve(grid: HexGrid, start_aspects: List[Tuple[int, int]]) -> SolvingHexGrid
 
     nodes_to_connect = []
     seen_hexes = set()
-    current_start = start_aspects[1]  # change index to change rotation
-    while True:
-        seen_hexes.add(current_start)
-        neigh_a, neigh_b = closest_neighbors[current_start]
-        if neigh_a[0] not in seen_hexes:
-            nodes_to_connect.append((current_start, neigh_a[0]))
-            current_start = neigh_a[0]
-        elif neigh_b[0] not in seen_hexes:
-            nodes_to_connect.append((current_start, neigh_b[0]))
-            current_start = neigh_b[0]
-        else:
-            break
+    current_start = start_aspects[1]  # change index to change ring rotation
+    if len(start_aspects) == 2:
+        nodes_to_connect = [(start_aspects[0], start_aspects[1])]
+    else:
+        while True:
+            seen_hexes.add(current_start)
+            neigh_a, neigh_b = closest_neighbors[current_start]
+            if neigh_a[0] not in seen_hexes:
+                nodes_to_connect.append((current_start, neigh_a[0]))
+                current_start = neigh_a[0]
+            elif neigh_b[0] not in seen_hexes:
+                nodes_to_connect.append((current_start, neigh_b[0]))
+                current_start = neigh_b[0]
+            else:
+                break
+
+    # TODO: refactor so this fallback isn't needed
+    if len(nodes_to_connect) != len(start_aspects) - 1:
+        for aspect_loc in start_aspects:
+            if not any(aspect_loc in conn for conn in nodes_to_connect):
+                neighbor, _ = closest_neighbors[aspect_loc][0]
+                nodes_to_connect.append((neighbor, aspect_loc))
 
     # (type[], (x, y)[])[][]
     # different source-destination ; different alternate paths ; different steps of path
